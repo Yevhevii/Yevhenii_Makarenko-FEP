8cf6131434afd07edc7c2ac5d2e9f5636ce43322
Yevhenii <makarenko.evg.ig@gmail.com>
2023-07-18 19:49:19 +0300

    Added to full homework

--- lesson_02/script.js
+++ lesson_02/script.js
@@ -1,51 +1,44 @@
 firstNumber = +prompt(`Enter a negative number`);
 firstNumber = +firstNumber.toFixed(2);
-console.log(firstNumber);
 secondNumber = +prompt(`Enter a positive number`);
 secondNumber = +secondNumber.toFixed(2);
-console.log(secondNumber);
-min = firstNumber;
-max = secondNumber;
-randomNumber = Number(Math.random() * (max - min + 1) + min);
-console.log(randomNumber);
+randomNumber = Number(Math.random() * (secondNumber - firstNumber + 1) + secondNumber);
 randomNumber = +randomNumber.toFixed(2);
-console.log(randomNumber);
 alert(`Random number is: ${randomNumber}`);
 thirdNumber = +prompt(`Enter third number`);
-console.log(thirdNumber);
 thirdNumber = +thirdNumber.toFixed(2);
-console.log(thirdNumber);
 sum = randomNumber + thirdNumber;
+sum = +sum.toFixed(2);
 subtraction = randomNumber - thirdNumber;
+subtraction = +subtraction.toFixed(2);
 multiplication = randomNumber * thirdNumber;
+multiplication = +multiplication.toFixed(2);
 division = randomNumber / thirdNumber;
-console.log(sum, subtraction, multiplication, division);
-document.write(<table>
+division = +division.toFixed(2);
+document.write(`<table>
     <tr>
-        <thead>
-            <th>Operation</th>
-            <th>Expression</th>
-            <th>Value</th>
-        </thead>
+        <th class = "title">Operation</th>
+        <th class = "title">Expression</th>
+        <th class = "title">Value</th>
     </tr>
     <tr>
-        <th>Sum</th>
-        <th>randomNumber + thirdNumber</th>
-        <th>sum</th>
+        <th class = "operation">Sum</th>
+        <th>${randomNumber} + ${thirdNumber}</th>
+        <th>${sum}</th>
     </tr>
     <tr>
-        <th>Subtraction</th>
-        <th></th>
-        <th>subtraction</th>
+        <th class = "operation">Subtraction</th>
+        <th>${randomNumber} - ${thirdNumber}</th>
+        <th>${subtraction}</th>
     </tr>
     <tr>
-        <th>Multiplication</th>
-        <th></th>
-        <th>multiplication</th>
+        <th class = "operation">Multiplication</th>
+        <th>${randomNumber} * ${thirdNumber}</th>
+        <th>${multiplication}</th>
     </tr>
     <tr>
-        <th>Division</th>
-        <th></th>
-        <th>division</th>
+        <th class = "operation">Division</th>
+        <th>${randomNumber} / ${thirdNumber}</th>
+        <th>${division}</th>
     </tr>
-</table>)
+</table>`);
