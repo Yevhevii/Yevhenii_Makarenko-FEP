424e7b5171e132bdca69eb9f49f7177cf8ad68cb
Yevhenii <makarenko.evg.ig@gmail.com>
2023-07-22 13:28:59 +0300

    Full hw2

--- lesson_03/script.js
+++ lesson_03/script.js
@@ -17,28 +17,38 @@ else {
     userLastName = userLastName.trim();
 }
 userFullName = `${userName} ${userLastName}`;
+
 userEmail = prompt(`Enter your email`);
-if (userEmail === null){
+if (userEmail.replaceAll(` `, ``) === ``){
     userEmail = `email not defined`;
-} else if (userEmail === ``){
+}else if (userEmail === null){
     userEmail = `email not defined`;
-} else if (!userEmail.includes(`@`)){
-    userEmail = `not valid email(symbol @ not exist)`;
-} else if (userEmail.startsWith(`@`)){
-    userEmail = `not valid email(symbol @ find in first place)`;
-} else if (userEmail.endsWith(`@`)){
-    userEmail = `not valid email(symbol @ find in last place)`;
-} else if (userEmail.startsWith(`@`) && userEmail.endsWith(`@`)){
+}
+else if (userEmail.startsWith(`@`) && userEmail.endsWith(`@`)){
     userEmail = `not valid email(symbol @ find in first place and last place)`;
-} else {
+}
+else if (!userEmail.includes(`@`)){
+    userEmail = `not valid email(symbol @ not exist)`;
+}else if (userEmail.startsWith(`@`)){
+    userEmail = `not valid email(symbol @ find in first place)`;
+}else if (userEmail.endsWith(`@`)){
+    userEmail = `not valid email(symbol @ find in last place)`;
+}else {
     userEmail = userEmail.replaceAll(` `, ``).toLowerCase();
 }
+
 yearBirth = prompt(`Enter your year of birth`);
-if (yearBirth === ``){
+if (yearBirth.replaceAll(` `, ``) === ``){
     yearBirth = `1997`;
 } else if (yearBirth === null){
     yearBirth = `1997`;
 } else {
-    yearBirth = yearBirth.replaceAll(` `, ``);
+    yearBirth = +yearBirth.replaceAll(` `, ``);
 }
 userAge = new Date().getFullYear() - yearBirth;
+
+document.write(`<ul>
+<li>Full name: ${userFullName}</li>
+<li>Email: ${userEmail}</li>
+<li>Age: ${userAge}</li>
+</ul>`);
