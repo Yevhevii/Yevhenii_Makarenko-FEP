fcae762c00bfecd776d02346717f3003e6b66b02
Yevhenii <makarenko.evg.ig@gmail.com>
2023-08-07 20:42:13 +0300

    Full hw5

--- HomeWork_05/script.js
+++ HomeWork_05/script.js
@@ -1,18 +1,23 @@
-sizeArray = +prompt(`Enter the length of the array from 2 to 10`);
-debugger
+sizeArray = Math.abs(Math.round(+prompt(`Enter the length of the array from 2 to 10`)));
 while(isNaN(sizeArray) 
-|| sizeArray === null 
-|| sizeArray.replaceAll(" ","") === ""){
-    sizeArray = prompt(`Enter the length of the array from 2 to 10`);
+|| sizeArray === null
+|| sizeArray < 2
+|| sizeArray >  10){
+    sizeArray = Math.abs(Math.round(+prompt(`Enter the length of the array from 2 to 10`)));
 }
-console.log(sizeArray);
-// if (sizeArray<0) sizeArray = Math.abs(sizeArray);
-// sizeArray = Math.round(sizeArray);
-// while (isNaN(sizeArray) 
-//     || sizeArray === null 
-//     || sizeArray.replaceAll(" ","") === ""
-//     || sizeArray <2 
-//     || sizeArray >10){
-//     sizeArray = prompt(`Enter the length of the array from 2 to 10`);
-// }
-// console.log(sizeArray);
+arr = new Array(+`${sizeArray}`);
+minRandom = -10;
+maxRandom = 10;
+for(i=0; i<arr.length; i++){
+    arr[i] = Math.floor(Math.random() * (maxRandom - minRandom + 1)) + minRandom;
+}
+for(i=1, min=max=arr[0]; i<arr.length; i++){
+currentElement = arr[i];
+if(currentElement < min) min = currentElement;
+if(currentElement > max) max = currentElement;
+}
+for(i=0, sum = 0; i<arr.length; i++) {sum+=arr[i];}
+console.log(arr);
+console.log(`Min element is ${min}`);
+console.log(`Max element is ${max}`);
+console.log(`Sum of arr elements is ${sum}`);
