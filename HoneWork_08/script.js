9872643e2f4864f114f2a238584c88bad1901c7c
Yevhenii <makarenko.evg.ig@gmail.com>
2023-08-14 20:45:18 +0300

    Full hw8

--- HoneWork_08/script.js
+++ HoneWork_08/script.js
@@ -6,8 +6,8 @@ animals = [
 	['🐻','bear','Paddington']
 ];
 
-function getInfo(animalsArrey,animalsTitle) {
-    animalsTitle = `<h3>Animals info</h3>`;
+function getInfo(animalsArrey,animalsHeding) {
+    animalsTitle = `<h3>${animalsHeding}</h3>`;
     trs = [];
 for(i=0; i<animalsArrey.length; i++){
     animalsItem = animalsArrey[i];
@@ -21,7 +21,7 @@ for(i=0; i<animalsArrey.length; i++){
     return `<h3>${animalsTitle}</h3>
     <table>${trs.join(``)}</table>`;
 }
-animalsTable = getInfo(animals, `Animals`);
+animalsTable = getInfo(animals, `Animals info`);
 animalsInfo = `<div>${animalsTable}</div>`
 
 food = [
@@ -32,8 +32,8 @@ food = [
 	['🍌','banana',7]
 ];
 
-function getFruits(foodsArrey, foodsTitle) {
-    foodsTitle = `<h3>Food info</h3>`;
+function getFruits(foodsArrey, foodsHeding) {
+    foodsTitle = `<h3>${foodsHeding}</h3>`;
     trs = [];
 for(i=0; i<foodsArrey.length; i++){
     foodsItem = foodsArrey[i];
@@ -47,7 +47,7 @@ for(i=0; i<foodsArrey.length; i++){
     return `<h3>${foodsTitle}</h3>
     <table>${trs.join(``)}</table>`;
 }
-foodsTable = getFruits(food, `Food`);
+foodsTable = getFruits(food, `Food info`);
 foodsInfo = `<div>${foodsTable}</div>`
 
 document.write(`<div class="table-info">
