1de296b2445f7390166a62932b4e63fde71bf997
Yevhenii <makarenko.evg.ig@gmail.com>
2023-08-27 19:03:58 +0300

    Version 2

--- HoneWork_10/script.js
+++ HoneWork_10/script.js
@@ -6,19 +6,27 @@ const animalsList =  [
     [`penguin`, `🐧`],
 ];
 
-let array = anyArray =>{
-	let arr = JSON.parse(JSON.stringify(anyArray));
-	let renderList=[];
-	for(let i=0; i<arr.length; i++){
-		let arrItem = arr[i];
-		renderList.push(`<p>${arrItem.join(`: `)}</p>`);
-	}
-	return renderList;
-}
-array(animalsList);
+// let array = anyArray =>{
+// 	let arr = JSON.parse(JSON.stringify(anyArray));
+// 	let renderList=[];
+// 	for(let i=0; i<arr.length; i++){
+// 		let arrItem = arr[i];
+// 		renderList.push(`<p>${arrItem.join(`: `)}</p>`);
+// 	}
+// 	return renderList;
+// }
+// array(animalsList);
 
-renderList.forEach((animals, i) => {
-	setTimeout(() => {
-	  document.write(animals);
-	}, i * 1000);
-  });
+// renderList.forEach((animals, i) => {
+// 	setTimeout(() => {
+// 	  document.write(animals);
+// 	}, i * 1000);
+//   });
+const renderItems = item => `<p>${item.join(`: `)}</p>`;
+let time = 1000;
+const renderItem = (list, time) => {
+	list.forEach((item, index) =>{
+        setTimeout(()=>document.write(renderItems(item)), ++index*time);
+    })
+}
+renderItem(animalsList, time);
