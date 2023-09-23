de3254cbc7635c591fae6574cedadba8d724bf42
Yevhenii <makarenko.evg.ig@gmail.com>
2023-09-23 22:45:02 +0300

    Full hw16

---
+++ HomeWork_16/script.js
@@ -0,0 +1,22 @@
+let btns = document.querySelectorAll(`button`);
+btns = [...btns];
+
+const firstFunc = (e)=>{
+    console.log(`First func for ${e.target.textContent}`);
+    e.target.removeEventListener(`click`, firstFunc);
+    e.target.addEventListener(`click`, secondFunc);
+}
+
+const secondFunc = (e)=>{
+    console.log(`Second func for ${e.target.textContent}`);
+    e.target.removeEventListener(`click`, secondFunc);
+    e.target.addEventListener(`click`, lastFunc);
+}
+
+const lastFunc = (e)=>{
+    console.log(`The last func for ${e.target.textContent}`);
+}
+
+btns.map((btn)=>{
+    btn.addEventListener(`click`, firstFunc);
+})
