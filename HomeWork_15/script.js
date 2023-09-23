13e8e86578c4055f0b60ffc67cab506761bd11c4
Yevhenii <makarenko.evg.ig@gmail.com>
2023-09-23 21:12:26 +0300

    Сhanges in hw_15

--- HomeWork_15/script.js
+++ HomeWork_15/script.js
@@ -1,35 +1,36 @@
 const BLOCK = document.querySelector(`.block`);
+let maxMoveBlockLeft, maxMoveBlockTop;
+
+function getRandomIntInclusive(min = 0, max = 255) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const randomColor = ()=>{
+    first = getRandomIntInclusive();
+    second = getRandomIntInclusive();
+    third = getRandomIntInclusive();
+    return `rgb(${first}, ${second}, ${third})`
+}
+
+const randomMovingBlockLeft = (()=>{
+    min = 0;
+    max = maxMoveBlockLeft;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+})
+
+const randomMovingBlockTop = (()=>{
+    min = 0;
+    max = maxMoveBlockTop;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+})
 
 setInterval(()=>{
     const bodyWidth = document.body.clientWidth;
     const bodyHeight = document.body.clientHeight;
-    const maxMoveBlockLeft = bodyWidth - BLOCK.clientWidth;
-    const maxMoveBlockTop = bodyHeight - BLOCK.clientHeight;
-
-    function getRandomIntInclusive(min = 0, max = 255) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    
-    const randomColor = ()=>{
-        first = getRandomIntInclusive();
-        second = getRandomIntInclusive();
-        third = getRandomIntInclusive();
-        return `rgb(${first}, ${second}, ${third})`
-    }
-    
-    const randomMovingBlockLeft = (()=>{
-        min = 0;
-        max = maxMoveBlockLeft;
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    })
-    
-    const randomMovingBlockTop = (()=>{
-        min = 0;
-        max = maxMoveBlockTop;
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    })
+    maxMoveBlockLeft = bodyWidth - BLOCK.clientWidth;
+    maxMoveBlockTop = bodyHeight - BLOCK.clientHeight;
 
     BLOCK.style.background = randomColor();
     BLOCK.style.top = randomMovingBlockTop() + `px`;
